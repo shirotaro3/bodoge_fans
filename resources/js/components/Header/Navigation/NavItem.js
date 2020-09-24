d3ee549803ef953ae1cb773282f51100008af4a2
Yusuke Shirota <y.shiro9102@gmail.com>
2020-09-24 11:13:43 +0900

    [add]react-icons導入

--- resources/js/components/Header/Navigation/NavItem.js
+++ resources/js/components/Header/Navigation/NavItem.js
@@ -3,9 +3,10 @@ import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 import { mixinDivLink } from '../../shared/StyledCss';
 
-const NavItem = ({to, children, className}) => {
+const NavItem = ({to, children, icon, className}) => {
   return (
     <div className={className}>
+      <div>{icon}</div>
       {children}
       <NavLink
         to={to}
@@ -18,13 +19,14 @@ const NavItem = ({to, children, className}) => {
 
 const StyledNavItem = styled(NavItem)`
   ${mixinDivLink}
+  color: #fff;
   width: 110px;
   transition: .6s;
   text-align: center;
-  padding-top: 19px;
-  font-size: 15px;
+  padding-top: 7px;
+  font-size: 13px;
   border-top: 7px solid #999;
-  border-bottom: 2px solid #999;
+  border-bottom: 2px solid #777;
   border-radius: 7px;
   ${StyledNavItem}:hover&{
     background: #555;
--- resources/js/components/Header/Navigation/index.js
+++ resources/js/components/Header/Navigation/index.js
@@ -2,30 +2,37 @@ import React from 'react';
 import styled from 'styled-components';
 import NavItem from './NavItem';
 import { mixinIsShownTablet } from '../../shared/StyledCss';
+import { BsSearch, BsFillCalendarFill } from 'react-icons/bs';
+import { IconContext } from 'react-icons';
 
 const items = [
   {
     to: '/search',
-    text: 'お店を探す'
+    text: 'お店を探す',
+    icon: <BsSearch />
   },
   {
     to: '/users/registration',
-    text: '新規登録'
+    text: 'イベント情報',
+    icon: <BsFillCalendarFill />
   }
 ]
 
 const Navigation = ({className}) => {
   return (
     <div className={className}>
-      {
-        items.map((item) => {
-          return (
-            <NavItem
-              to={item.to}
-              key={item.to}
-            >{item.text}</NavItem>)
-        })
-      }
+      <IconContext.Provider value={{ size: 18, style: { margin: '0 auto 4px auto' } }}>
+        {
+          items.map((item) => {
+            return (
+              <NavItem
+                to={item.to}
+                key={item.to}
+                icon={item.icon}
+              >{item.text}</NavItem>)
+          })
+        }
+      </IconContext.Provider>
     </div>
   );
 }
