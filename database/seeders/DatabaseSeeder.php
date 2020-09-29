d063414ca9e4f7464c27b29e419c1daded9e8f74
Yusuke Shirota <y.shiro9102@gmail.com>
2020-09-29 19:06:17 +0900

    [add]MFacilityTypeSeeder追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(MBudgetSeeder::class);
+        $this->call(MFacilityTypeSeeder::class);
     }
 }
---
+++ database/seeders/MFacilityTypeSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class MFacilityTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('m_facility_types')->insert([
+            'detail' => 'カフェ',
+        ]);
+        DB::table('m_facility_types')->insert([
+            'detail' => 'バー・居酒屋',
+        ]);
+        DB::table('m_facility_types')->insert([
+            'detail' => 'レストラン',
+        ]);
+        DB::table('m_facility_types')->insert([
+            'detail' => 'イベントホール',
+        ]);
+        DB::table('m_facility_types')->insert([
+            'detail' => 'ショップ',
+        ]);
+    }
+}
