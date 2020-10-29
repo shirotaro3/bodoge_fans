9ebd0d38c215e9360bb60871ba0b67eb3a34afbd
Yusuke Shirota <winterleaves2219.443@gmail.com>
2020-10-30 04:12:52 +0900

    [add]PWA仮対応

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
         <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> 
-        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+        <link rel="manifest" href="manifest.json" />
 
         <!-- Styles -->
         <style>
@@ -30,6 +30,7 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
         <script>
             var sessionUser = @json($user);
+            navigator.serviceWorker.register("/serviceWorker.js");
         </script>
     </body>
 </html>
